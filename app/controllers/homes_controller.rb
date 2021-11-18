0710833b9c92f777901fc9a64c1ddae1e5cf49ff
RyotaAbe-1 <ryota.a.0517@gmail.com>
2021-11-18 10:45:49 +0000

    [Add]介護保険算出の仕組み

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -13,5 +13,14 @@ class HomesController < ApplicationController
     end
     @welfare_pension = @standard_reward_bymonth_wp * 18.3 / 100 / 2
     @helth_insurance = @standard_reward_bymonth_hi * 9.84 / 100 / 2
+    
+    age_range = params[:age_range]
+    if age_range == "40_to_65"
+      @standard_reward_bymonth_ni = InsuranceFee.where("first_range <= ?", face_value).last.standard_reward_bymonth
+      @nursing_insurance = @standard_reward_bymonth_ni * 1.8 / 100 / 2
+    elsif age_range == "and_over_65"
+      flash[:notice] = "前年の所得や市区町村ごとに変化します。全国平均は6014円/月です。"
+    end
+      
   end
 end
