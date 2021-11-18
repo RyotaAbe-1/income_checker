246a2725bd2e685dfc57e9c751ead6ec35462716
RyotaAbe-1 <ryota.a.0517@gmail.com>
2021-11-18 10:06:31 +0000

    [Add]健康保険料と厚生年金保険料算出の仕組み

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -4,10 +4,14 @@ class HomesController < ApplicationController
 
   def result
     face_value = params[:face_value].to_i
-    if face_value < 1355000
-      @standard = InsuranceFee.where("first_range <= ? and last_range > ?", face_value, face_value).first
+    if face_value >= 635000
+      @standard_reward_bymonth_wp = InsuranceFee.where("first_range >= ?", 635000).first.standard_reward_bymonth
+      @standard_reward_bymonth_hi = InsuranceFee.where("first_range <= ?", face_value).last.standard_reward_bymonth
     else
-      @standard = InsuranceFee.where("first_range <= ?", face_value).first
+      @standard_reward_bymonth_hi = InsuranceFee.where("first_range <= ?", face_value).last.standard_reward_bymonth
+      @standard_reward_bymonth_wp = InsuranceFee.where("first_range <= ?", face_value).last.standard_reward_bymonth
     end
+    @welfare_pension = @standard_reward_bymonth_wp * 18.3 / 100 / 2
+    @helth_insurance = @standard_reward_bymonth_hi * 9.84 / 100 / 2
   end
 end
